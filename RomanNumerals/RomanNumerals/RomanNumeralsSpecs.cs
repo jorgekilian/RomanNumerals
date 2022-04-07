b516f3eee65f95677a5ec2ff32ff0add3d0d543b
Kilian Suárez <ksuarez@domingoalonsogroup.com>
2022-04-07 17:53:00 +0100

    3

--- RomanNumerals/RomanNumerals/RomanNumeralsSpecs.cs
+++ RomanNumerals/RomanNumerals/RomanNumeralsSpecs.cs
@@ -6,7 +6,9 @@ namespace RomanNumeralsSpecs {
         public void Setup() {
         }
 
-        [TestCase(1,"I")]
+        [TestCase(1, "I")]
+        [TestCase(2, "II")]
+        [TestCase(3, "III")]
         public void calculate_the_roman_numeral_from_normal_number(int number, string roman) {
             string result = RomanNumeral.FromNumberToRoman(number);
             Assert.AreEqual(roman, result);
@@ -15,6 +17,8 @@ namespace RomanNumeralsSpecs {
 
     public class RomanNumeral {
         public static string FromNumberToRoman(int number) {
+            if (number == 3) return "III";
+            if (number == 2) return "II";
             return "I";
         }
     }
